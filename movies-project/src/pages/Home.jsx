4360bea036a8f289dba226c3f1ba2bb8a0d51216
André Albson <andre.albson@precode.com.br>
2024-04-23 11:59:18 -0300

    Add loading spinner and improve card styling
    A loading spinner has been added to the Home page to inform users when content is being retrieved. The styling of movie cards has been improved, including the addition of hover effects in the cards-topRated class. The card titles also now truncate with an ellipsis for better readability.

--- movies-project/src/pages/Home.jsx
+++ movies-project/src/pages/Home.jsx
@@ -21,6 +21,13 @@ const Home = () => {
     return (
         <div className={'cards-topRated'}>
             <div className={'container-fluid'}>
+                {(topMovies.length === 0) &&
+                    <div className={'row justify-content-center'}>
+                        <div className="spinner-border text-danger text-center" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                }
                 <div className="row row-cols-lg-6 row-cols-md-6 col-lg-612 g-4">
                     {topMovies && topMovies.map((movie, index) => {
                             return (
@@ -29,7 +36,7 @@ const Home = () => {
                                         <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title}
                                              className="card-img-top"/>
                                         <div className="card-body">
-                                            <h5 className="card-title">{movie.title}</h5>
+                                            <h5 className="card-title fs-6">{movie.title}</h5>
                                             <p className="card-text truncate-text">{movie.overview}</p>
                                         </div>
                                         <div className="card-footer">
